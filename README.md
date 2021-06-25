a67b09b496cba7dfe9b876e340cc00e5a6c183e7
Bowman Klinkenberg <bowman.klinkenberg@target.com>
2021-06-25 11:22:07 -0500

    lic to gplv3, moved main to funcs

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # algo-learn
+
+Exercises from https://developer.algorand.org/docs/build-apps/
--- main.go
+++ main.go
@@ -15,57 +15,41 @@ type Config struct {
 	Url string `mapstructure:"ALGO_URL"`
 }
 
-// LoadConfig reads configuration from file or environment variables.
-func LoadConfig() (c Config, err error) {
+// loadConfig reads configuration from file or environment variables.
+func loadConfig() (config Config, err error) {
 	viper.AutomaticEnv()
-
-	err = viper.BindEnv("key", "ALGO_KEY")
+	err = viper.BindEnv("ALGO_KEY")
 	if err != nil {
-		return
+		return Config{}, err
 	}
-	err = viper.BindEnv("url", "ALGO_URL")
+	viper.SetDefault("ALGO_KEY", "")
+	err = viper.BindEnv("ALGO_URL")
 	if err != nil {
-		return
+		return Config{}, err
 	}
-	fmt.Println(viper.AllSettings())
-
-	err = viper.UnmarshalExact(&c)
+	viper.SetDefault("ALGO_URL", "https://testnet-algorand.api.purestake.io/ps2/")
+	err = viper.UnmarshalExact(&config)
 	if err != nil {
 		log.Fatal("unable to decode into struct, %v", err)
 	}
-	return c, nil
+	return config, nil
 }
 
-func main() {
-
-	//config, err := LoadConfig()
-	//if err != nil {
-	//	log.Fatal("cannot load config:", err)
-	//}
-	config := new(Config)
-	viper.AutomaticEnv()
-	viper.BindEnv("ALGO_KEY")
-	viper.SetDefault("ALGO_KEY", "foo")
-	viper.BindEnv("ALGO_URL")
-	viper.SetDefault("ALGO_URL", "https://testnet-algorand.api.purestake.io/ps2/")
-	viper.ReadInConfig()
-	fmt.Println(viper.AllSettings())
-	err := viper.UnmarshalExact(&config)
-	if err != nil {
-		log.Fatal("unable to decode into struct, %v", err)
-	}
-
+// newClient creates a new Algod client using standard settings
+func newClient(config Config) (client* algod.Client) {
 
 	commonClient, err := common.MakeClient(config.Url, "X-API-Key", config.Key)
 	if err != nil {
 		fmt.Printf("Issue with creating algod client: %s\n", err)
 		return
 	}
+	// TODO: add checks for catchup-time and validation against block explorer
+	return (*algod.Client)(commonClient)
+}
 
-	algodClient := (*algod.Client)(commonClient)
-
-	// print node status
-	status, err := algodClient.Status().Do(context.Background())
+// getStatus returns a JSON-formatted status for the client connected node
+func getStatus(client *algod.Client) (s string) {
+	status, err := client.Status().Do(context.Background())
 	if err != nil {
 		fmt.Printf("Error getting status: %s\n", err)
 		return
@@ -74,11 +58,12 @@ func main() {
 	if err != nil {
 		fmt.Printf("Can not marshall status data: %s\n", err)
 	}
-	fmt.Println("Connected node status:")
-	fmt.Printf("%s\n", statusJSON)
+	return string(statusJSON)
+}
 
-	// suggested transaction params
-	txParams, err := algodClient.SuggestedParams().Do(context.Background())
+// getTxParams returns JSON-formatted txparams
+func getTxParams(client *algod.Client) (s string){
+	txParams, err := client.SuggestedParams().Do(context.Background())
 	if err != nil {
 		fmt.Printf("Error Algorand suggested parameters: %s\n", err)
 		return
@@ -87,7 +72,23 @@ func main() {
 	if err != nil {
 		fmt.Printf("Can not marshall suggested parameters data: %s\n", err)
 	}
+	return string(JSON)
+}
+
+func main() {
+	config, err := loadConfig()
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+
+	algodClient := newClient(config)
+
+	// print status
+	fmt.Println("Connected node status:")
+	fmt.Println(getStatus(algodClient))
+
+	// suggested transaction params
 	fmt.Println("Suggested transaction parameters:")
-	fmt.Printf("%s\n", JSON)
+	fmt.Println(getTxParams(algodClient))
 
 }
