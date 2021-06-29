6922a30a6e13961cf7c1c4a78a584a1e2b5c8066
Bowman Klinkenberg <bowman.klinkenberg@target.com>
2021-06-29 15:25:46 -0500

    added transaction verify

--- accounts.go
+++ accounts.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"errors"
 	"fmt"
 	"github.com/algorand/go-algorand-sdk/client/v2/algod"
+	"github.com/algorand/go-algorand-sdk/client/v2/common/models"
 	"github.com/algorand/go-algorand-sdk/crypto"
 	"github.com/algorand/go-algorand-sdk/mnemonic"
 	"github.com/algorand/go-algorand-sdk/transaction"
 	"github.com/algorand/go-algorand-sdk/types"
+	"strings"
 )
 
 // newStandaloneAccount creates a standalone account and prints the address and mnemonic
@@ -41,7 +45,7 @@ func acctInfo(client *algod.Client, config Config) {
 	fmt.Printf("Account balance: %f Algos\n", algoBalance)
 }
 
-// sendFaucetPayment sends an example transaction from the configured address to the Algo faucet address
+// sendFaucetPayment sends an example transaction in micro algos from the configured address to the Algo faucet address
 func sendFaucetPayment(amount uint64, client *algod.Client, config Config) {
 	txParams, err := client.SuggestedParams().Do(context.Background())
 	if err != nil {
@@ -51,7 +55,6 @@ func sendFaucetPayment(amount uint64, client *algod.Client, config Config) {
 	// comment out the next two (2) lines to use suggested fees
 	txParams.FlatFee = true
 	txParams.Fee = 1000
-
 	fromAddr := config.pubAddress().String()
 	toAddr := "GD64YIY3TWGDMCNPP553DZPPR6LDUSFQOIJVFDPPXWEG3FVOJCCDBBHU5A"
 	var minFee uint64 = 1000
@@ -81,4 +84,62 @@ func sendFaucetPayment(amount uint64, client *algod.Client, config Config) {
 		return
 	}
 	fmt.Printf("Submitted transaction %s\n", sendResponse)
+
+	// Wait for confirmation
+	confirmedTxn, err := waitForConfirmation(txID, client, 5)
+	if err != nil {
+		fmt.Printf("Error wating for confirmation on txID: %s\n", txID)
+		return
+	}
+	txnJSON, err := json.MarshalIndent(confirmedTxn.Transaction.Txn, "", "\t")
+	if err != nil {
+		fmt.Printf("Can not marshall txn data: %s\n", err)
+	}
+	fmt.Printf("Transaction information: %s\n", txnJSON)
+
+	fmt.Printf("Decoded note: %s\n", string(confirmedTxn.Transaction.Txn.Note))
+}
+
+// Function that waits for a given txId to be confirmed by the network
+func waitForConfirmation(txID string, client *algod.Client, timeout uint64) (models.PendingTransactionInfoResponse, error) {
+	pt := new(models.PendingTransactionInfoResponse)
+	if client == nil || txID == "" || timeout < 0 {
+		fmt.Printf("Bad arguments for waitForConfirmation")
+		var msg = errors.New("Bad arguments for waitForConfirmation")
+		return *pt, msg
+
+	}
+
+	status, err := client.Status().Do(context.Background())
+	if err != nil {
+		fmt.Printf("error getting algod status: %s\n", err)
+		var msg = errors.New(strings.Join([]string{"error getting algod status: "}, err.Error()))
+		return *pt, msg
+	}
+	startRound := status.LastRound + 1
+	currentRound := startRound
+
+	for currentRound < (startRound + timeout) {
+
+		*pt, _, err = client.PendingTransactionInformation(txID).Do(context.Background())
+		if err != nil {
+			fmt.Printf("error getting pending transaction: %s\n", err)
+			var msg = errors.New(strings.Join([]string{"error getting pending transaction: "}, err.Error()))
+			return *pt, msg
+		}
+		if pt.ConfirmedRound > 0 {
+			fmt.Printf("Transaction "+txID+" confirmed in round %d\n", pt.ConfirmedRound)
+			return *pt, nil
+		}
+		if pt.PoolError != "" {
+			fmt.Printf("There was a pool error, then the transaction has been rejected!")
+			var msg = errors.New("There was a pool error, then the transaction has been rejected")
+			return *pt, msg
+		}
+		fmt.Printf("waiting for confirmation\n")
+		status, err = client.StatusAfterBlock(currentRound).Do(context.Background())
+		currentRound++
+	}
+	msg := errors.New("Tx not found in round range")
+	return *pt, msg
 }
